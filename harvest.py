ecc885d1dd7a9b6c64b1fb8064ff3bb37decdfa8
Your Name <you@example.com>
2020-01-16 13:02:43 -0800

    Finished part 1 of lab

--- harvest.py
+++ harvest.py
@@ -71,7 +71,14 @@ def print_pairing_info(melon_types):
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    #Fill in the rest
+
+    melon_dict = {}
+    for melon_type in melon_types:
+        Melon_dict[melon_type.code] = melon_type
+
+    return melon_dict
+
 
 ############
 # Part 2   #
